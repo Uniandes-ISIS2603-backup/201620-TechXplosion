7a49dc520e5da2a5a42e6e5f3e940af875ff1594
js.numpaque10 <js.numpaque10@ISIS2603-05.sis.virtual.uniandes.edu.co>
2016-08-15 01:00:05 -0500

    Clase que modela el recurso Reserva del modelo del mundo.

---
+++ src/main/java/co/edu/uniandes/rest/cities/dtos/ReservaDTO.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.rest.cities.dtos;
+
+import java.util.Date;
+
+/**
+ *
+ * @author js.numpaque10
+ */
+public class ReservaDTO 
+{
+    private Long id;
+    private Long idUsuario;
+    private Long idRecurso;
+    private Date fechaSolicitud;
+    
+    /**
+     * Constructor por defecto
+     */
+    public ReservaDTO(){
+        }
+    
+    /**
+     * 
+     * @param id Identificador de la reserva.
+     * @param idUsuario Identificador de quien realiza la reserva.
+     * @param idRecurso Identificador del recurso reservado.
+     * @param fechaSolicitud Fecha de reserva.
+     */
+    public ReservaDTO (Long id, Long idUsuario, Long idRecurso, Date fechaSolicitud)
+    {
+        super();
+        this.id = id;
+        this.idUsuario = idUsuario;
+        this.idRecurso = idRecurso;
+        this.fechaSolicitud = fechaSolicitud;
+    }
+    
+    /**
+     * @return El id de la reserva.
+     */
+    public Long getId() {
+        return id;
+    }
+    
+    /**
+     * @param id El id de la reserva. 
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    /**
+     * @return El id del usuario que hizo la reserva.
+     */
+    public Long getIdUsuario() {
+        return idUsuario;
+    }
+    
+    /**
+     * @param idUsuario El id del usuario que hizo la reserva.
+     */
+    public void setIdUsuario(Long idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+    
+    /**
+     * @return El id del recurso que fue reservado.
+     */
+    public Long getIdRecurso() {
+        return idRecurso;
+    }
+    
+    /**
+     * @param idRecurso El id del recurso que fue reservado.
+     */
+    public void setIdRecurso(Long idRecurso) {
+        this.idRecurso = idRecurso;
+    }
+    
+    /**
+     * @return Fecha de solicitud de la reserva.
+     */
+    public Date getFechaSolicitud() {
+        return fechaSolicitud;
+    }
+    /**
+     * @param fechaSolicitud Fecha de solicitud de la reserva.
+     */
+    public void setFechaSolicitud(Date fechaSolicitud) {
+        this.fechaSolicitud = fechaSolicitud;
+    }
+    
+    /**
+     * Convierte el objeto a una cadena
+     */
+    @Override
+    public String toString() {
+        return "{ id: " + getId() + ", idUsuario : \"" + getIdUsuario() + "\"" + ", idRecurso : \"" + getIdRecurso() + "\"" + ", fechaSolicitud : \"" + getFechaSolicitud()+"\"}";
+    }
+  
+}
+
