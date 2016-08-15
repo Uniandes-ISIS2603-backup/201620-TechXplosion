50c58c7488971e33f0c6e5c62fb7d4ac6a53d541
js.numpaque10 <js.numpaque10@ISIS2603-05.sis.virtual.uniandes.edu.co>
2016-08-15 00:58:39 -0500

    ReservaResource con sus respectivas invocaciones a ReservaMock

---
+++ src/main/java/co/edu/uniandes/rest/cities/resources/ReservaResource.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.rest.cities.resources;
+
+import co.edu.uniandes.rest.cities.dtos.ReservaDTO;
+import co.edu.uniandes.rest.cities.exceptions.CityLogicException;
+import co.edu.uniandes.rest.cities.mocks.ReservaMock;
+import java.util.Date;
+import java.util.List;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+/**
+ *
+ * @author js.numpaque10
+ */
+@Path("reservas")
+@Produces("application/json")
+public class ReservaResource {
+    
+    ReservaMock reservaMock = new ReservaMock();
+    
+    /**
+     * Obtiene el listado de reservas
+     * @return Lista de reservas
+     * @throws CityLogicException 
+     */
+    @GET
+    public List<ReservaDTO> getReservas() throws CityLogicException
+    {
+        return reservaMock.getReservas();
+    }
+    
+    /**
+     * Retorna una reserva con un id dado
+     * @param id El id de la reserva que se desea obtener
+     * @return La reserva que se deseaba obtener
+     * @throws CityLogicException En caso de no haber una reserva con el ida dado o que la lista de reservas sea vacía.
+     */
+    @GET
+    @Path("{id: \\d+}")
+    public ReservaDTO getReserva(@PathParam("id") Long id) throws CityLogicException 
+    {
+        return reservaMock.getReserva(id);
+    }
+   
+    /**
+     * Obtiene las reservas que tiene un recurso en especifico.
+     * @param idRecurso El id del recurso del cual queremos obtener sus reservas.
+     * @return Lista de reservas que tiene un recurso dado.
+     * @throws CityLogicException En caso de que no hayan reservas para ese recurso o que la lista de reservas esté vacía.
+     */
+    @GET
+    @Path("{idRecurso: \\d+}")
+    public List<ReservaDTO> getReservasPorRecurso(@PathParam("idRecurso")Long idRecurso) throws CityLogicException
+    {
+        return reservaMock.getReservasPorRecurso(idRecurso);
+    }
+    
+    /**
+     * Obtiene las reservas que tiene un usuario en especifico.
+     * @param idUsuario El id del usuario del cual queremos obtener sus reservas.
+     * @return Lista de reservas que tiene un usuario dado.
+     * @throws CityLogicException En caso de que no hayan reservas para ese usuario o que la lista de reservas esté vacía.
+     */
+    @GET
+    @Path("{idUsuario: \\d+}")
+    public List<ReservaDTO> getReservasPorUsuario(@PathParam("idUsuario") Long idUsuario) throws CityLogicException
+    {
+        return reservaMock.getReservasPorUsuario(idUsuario);
+    }
+    
+    /**
+     * Obtiene el listado de reservas hechas en una fecha dada
+     * @param fechaSolicitud Fecha por la cual se desea buscar una reserva.
+     * @return Lista de reservas hechas en la fecha dada.
+     * @throws CityLogicException En caso de que no hayan reservas hechas para esa fecha o que la lista de reservas sea vacía.
+     */
+    @GET
+    @Path("{fechaSolicitud: \\d+}")
+    public List<ReservaDTO> getReservasPorFecha( @PathParam("fechaSolicitud") Date fechaSolicitud ) throws CityLogicException
+    {
+        return reservaMock.getReservasPorFecha(fechaSolicitud);
+    }
+    
+    /**
+     * Agrega una reserva
+     * @param nuevaReserva Reserva a agregar.
+     * @return La reserva agregada.
+     * @throws CityLogicException Cuando ya hay una reserva con el mismo id.
+     */
+    @POST
+    public ReservaDTO createReserva(ReservaDTO nuevaReserva) throws CityLogicException
+    {
+        return reservaMock.createReserva(nuevaReserva);
+    }
+    
+    /**
+     * Elimina un recurso dado de la lista de reservas.
+     * @param id El id del recurso que se desea eliminar.
+     * @throws CityLogicException Si no encuentra un recurso con el id dado o si la lista de reservas está vacía.
+     */
+    @DELETE
+    @Path("{id: \\d+}")
+    public void deleteReserva(@PathParam("id") Long id) throws CityLogicException
+    {
+      reservaMock.deleteReserva(id);
+    }
+    /**
+     * Actualiza una instancia de la entidad Reserva.
+     * @param id Id de la instancia que se quiere actualizar.
+     * @param reservaMod La instancia de Reserva actualizada.
+     * @return La instancia de Reserva actualizada.
+     * @throws CityLogicException Si la lista de reservas es vacía, si no hay una reserva con el id dado o si el path y el id de la reserva no coinciden.
+    */
+    @PUT
+    @Path("{id: \\d+}")
+    public ReservaDTO updateReserva(@PathParam("id") Long id ,ReservaDTO reservaMod) throws CityLogicException
+    {
+        return reservaMock.updateReserva(id, reservaMod);
+    }
+    
+}
