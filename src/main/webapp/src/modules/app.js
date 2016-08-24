e61a1552db609b18b77e4dce83d7ca650bf19328
sapardo10 <sa.pardo10@sis.virtual.uniandes.edu.co>
2016-08-24 11:46:53 -0500

    creacion carpetas angular

---
+++ src/main/webapp/src/modules/app.js
@@ -0,0 +1,19 @@
+(function (ng) {
+
+    var mod = ng.module("mainApp", [
+        "ui.router",
+        "citiesModule",
+        "ngMessages"
+    ]);
+
+    mod.config(['$logProvider', function ($logProvider) {
+            $logProvider.debugEnabled(true);
+        }]);
+
+    mod.config(['$urlRouterProvider', function ($urlRouterProvider) 
+{
+            $urlRouterProvider.otherwise('/citiesList');
+        }]);
+
+  
+})(window.angular);
