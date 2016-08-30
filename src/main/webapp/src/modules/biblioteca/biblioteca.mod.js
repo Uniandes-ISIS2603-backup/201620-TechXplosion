216ef40029945414a12df40c989b2917de6c3811
Juan Sosa <js.sosa10@uninades.edu.co>
2016-08-30 10:12:08 -0500

    blogs y bibliotecas

--- src/main/webapp/src/modules/biblioteca/biblioteca.mod.js
+++ src/main/webapp/src/modules/biblioteca/biblioteca.mod.js
@@ -1,1 +1,41 @@
-var mod =ng.module("bibliotecaModule",["ui.router"]);
+(function (ng) {
+    var mod = ng.module("bibliotecaModule", ["ngMessages"]);
+    mod.constant("bibliotecaContext", "api/bibliotecas");
+    mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+            var basePath = 'src/modules/biblioteca/';
+            $urlRouterProvider.otherwise("/bibliotecasList");
+     
+            $stateProvider.state('bibliotecasList', {
+                url: '/bibliotecas',
+                views: {
+                    'mainView': {
+                        controller: 'bibliotecaCtrl',
+                        controllerAs: 'ctrl',
+                        templateUrl: basePath + "biblioteca.list.html"
+                    }
+                }
+            }).state('bibliotecaCreate', {
+                url: '/bibliotecas/create',
+                views: {
+                    'mainView': {
+                        controller: 'bibliotecaCtrl',
+                        controllerAs: 'ctrl',
+                        templateUrl: basePath + 'biblioteca.create.html'
+                    }
+                }
+
+            }).state('bibliotecaEdit', {
+                url: '/bibliotecas/:bibliotecaId',
+                param: {
+                    cityId: null
+                },
+                views: {
+                    'mainView': {
+                        controller: 'bibliotecaCtrl',
+                        controllerAs: 'ctrl',
+                        templateUrl: basePath + 'biblioteca.create.html'
+                    }
+                }
+            });
+        }]);
+})(window.angular);
