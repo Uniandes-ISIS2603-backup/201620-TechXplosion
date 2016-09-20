23e18beca570bc9d1beff3a578fd6572e3320f80
jm.rodriguez11 <jm.rodriguez11@ISIS2603-0107.sis.virtual.uniandes.edu.co>
2016-09-19 19:06:17 -0500

    remote ==

--- src/main/webapp/src/modules/usuario/usuario.ctrl.js
+++ src/main/webapp/src/modules/usuario/usuario.ctrl.js
@@ -41,7 +41,7 @@
                 currentRecord = $scope.currentRecord;
                 
                 // si el id es null, es un registro nuevo, entonces lo crea
-                if (id === null) {
+                if (id == null) {
 
                     // ejecuta POST en el recurso REST
                     return $http.post(context, currentRecord)
