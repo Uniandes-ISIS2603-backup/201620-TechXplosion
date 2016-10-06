3f6c87249d550819de4ccf5a2f68e5684ba227cc
juan camilo sanchez <jc.sanchez@uniandes.edu.co>
2016-10-06 12:01:07 -0500

    entity y persistance de lbro

--- bibliotecas-back/src/main/java/co/edu/uniandes/techxplosion/bibliotecas/persistence/LibroPersistence.java
+++ bibliotecas-back/src/main/java/co/edu/uniandes/techxplosion/bibliotecas/persistence/LibroPersistence.java
@@ -5,10 +5,28 @@
  */
 package co.edu.uniandes.techxplosion.bibliotecas.persistence;
 
+import co.edu.uniandes.techxplosion.bibliotecas.entities.LibroEntity;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 /**
  *
  * @author js.sosa10
  */
+@Stateless
 public class LibroPersistence {
+    
+     private static final Logger LOGGER = Logger.getLogger(LibroPersistence.class.getName());
+
+    @PersistenceContext(unitName = "TechxplosionPU")
+    protected EntityManager em;
+
+    public LibroEntity find(Long id) {
+        LOGGER.log(Level.INFO, "Consultando company con id={0}", id);
+        return em.find(LibroEntity.class, id);
+    }
     
 }
