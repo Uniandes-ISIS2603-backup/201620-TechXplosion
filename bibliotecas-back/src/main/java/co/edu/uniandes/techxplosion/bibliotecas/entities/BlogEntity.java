f97df81bc8a1fd69d0595a11f50a36428dddc5a3
Juan Sosa <js.sosa10@uninades.edu.co>
2016-10-05 21:31:25 -0500

    Se crearon los Entitys

---
+++ bibliotecas-back/src/main/java/co/edu/uniandes/techxplosion/bibliotecas/entities/BlogEntity.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.techxplosion.bibliotecas.entities;
+
+import java.io.*;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import uk.co.jemos.podam.common.PodamExclude;
+/**
+ *
+ * @author js.sosa10
+ */
+@Entity
+public class BlogEntity extends BaseEntity implements Serializable{
+   @PodamExclude 
+   @ManyToOne
+   private LibroEntity libro;
+   private String descripcion;
+   private String contenido;
+   /**
+    * Obtiene el atributo libro.
+    * @return atributo libro.
+    */
+   public LibroEntity getLibro(){
+       return libro;
+   }
+   /**
+    * Establece el valor del atributo libro.
+    * @param libro nuevo valor del atributo libro.
+    */
+   public void setLibro(LibroEntity libro){
+       this.libro=libro;
+   }
+   /**
+    * Obtiene el atributo descripcion.
+    * @return atributo descripcion.
+    */
+   public String getDescripcion(){
+       return descripcion;
+   }
+   /**
+    * Establece el valor del atributo descripcion.
+    * @param descripcion nuevo valor del atributo descripcion.
+    */
+   public void setDescripcion(String descripcion){
+       this.descripcion=descripcion;
+   }
+   /**
+    * Obtiene el valor del atributo contenido.
+    * @return atributo contenido.
+    */
+   public String getContenido(){
+       return contenido;
+   }
+   /**
+    * Establece el valor del atributo contenido.
+    * @param contenido nuevo valor del atributo contenido.
+    */
+   public void setContenido(String contenido){
+       this.contenido=contenido;
+   }
+}
---
+++ bibliotecas-back/src/main/java/co/edu/uniandes/techxplosion/bibliotecas/entities/MedioPagoEntity.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.techxplosion.bibliotecas.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import uk.co.jemos.podam.common.PodamExclude;
+
+/**
+ *
+ * @author js.sosa10
+ */
+@Entity
+public class MedioPagoEntity extends BaseEntity implements Serializable{
+   @PodamExclude 
+   @ManyToOne
+   private UsuarioEntity usuario;
+   private String tipo;
+   private int numero;
+   private int numeroSeguridad;
+   public UsuarioEntity getUsuario(){
+       return usuario;
+   }
+   public void setUsuario(UsuarioEntity usuario){
+       this.usuario=usuario;
+   }
+   public String getTipo(){
+       return tipo;
+   }
+   public void setTipo(String tipo){
+       this.tipo=tipo;
+   }
+   public int getNumero(){
+       return numero;
+   }
+   public void setNumero(int numero){
+        this.numero=numero;
+   }
+   public int getNumeroSeguridad(){
+       return numeroSeguridad;
+   }
+   public void setNumeroSeguridad(int numeroSeguridad){
+       this.numeroSeguridad=numeroSeguridad;
+   }
+}
