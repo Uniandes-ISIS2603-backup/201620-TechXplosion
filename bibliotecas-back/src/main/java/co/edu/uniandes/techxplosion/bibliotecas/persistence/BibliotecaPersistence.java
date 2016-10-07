ff23046dc585645a96064b0efd9c070f92d4a1b6
Juan Sosa <js.sosa10@uninades.edu.co>
2016-10-07 01:25:31 -0500

    BibliotecaPersistence.

--- bibliotecas-back/src/main/java/co/edu/uniandes/techxplosion/bibliotecas/persistence/BibliotecaPersistence.java
+++ bibliotecas-back/src/main/java/co/edu/uniandes/techxplosion/bibliotecas/persistence/BibliotecaPersistence.java
@@ -5,7 +5,14 @@
  */
 package co.edu.uniandes.techxplosion.bibliotecas.persistence;
 
+import co.edu.uniandes.techxplosion.bibliotecas.entities.BibliotecaEntity;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -13,5 +20,31 @@ import javax.ejb.Stateless;
  */
 @Stateless
 public class BibliotecaPersistence {
-    
+    private static final Logger LOGGER = Logger.getLogger(LibroPersistence.class.getName());
+    @PersistenceContext(unitName="TechxplsionPU")
+    protected EntityManager em;
+    public BibliotecaEntity fing(Long id){
+        LOGGER.log(Level.INFO,"Consulatndo Biblioteca con id={0}",id);
+        return em.find(BibliotecaEntity.class,id);
+    }
+    public List<BibliotecaEntity> findAll(){
+      LOGGER.info("Consulatndo todas las bibliotecas");
+      Query q = em.createQuery("select u from BibliotecaEntity u");
+      return q.getResultList();
+    }
+    public BibliotecaEntity create(BibliotecaEntity entity){
+        LOGGER.info("Creando una biblioteca nueva");
+        em.persist(entity);
+        LOGGER.info("Biblioteca creada");
+        return entity;
+    }
+    public BibliotecaEntity update(BibliotecaEntity entity){
+        LOGGER.log(Level.INFO,"Actualizando Biblioteca con id={0}",entity.getId());
+        return em.merge(entity);
+    }
+    public void delete(Long id){
+        LOGGER.log(Level.INFO,"Borrando biblioteca con id={0}",id);
+        BibliotecaEntity entity = em.find(BibliotecaEntity.class,id);
+        em.remove(entity);
+    }
 }
