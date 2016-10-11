f9439e8de4443cba71fe25b9977ffeb5482ead08
Nicolás Muñoz <nd.munoz10@uniandes.edu.co>
2016-10-11 07:27:08 -0500

    Implementacion de VideoEntity/VideoPersistance/Corrección errores varios

--- bibliotecas-back/src/main/java/co/edu/uniandes/techxplosion/bibliotecas/persistence/VideoPersistence.java
+++ bibliotecas-back/src/main/java/co/edu/uniandes/techxplosion/bibliotecas/persistence/VideoPersistence.java
@@ -13,56 +13,44 @@ import javax.ejb.Stateless;
 import javax.persistence.Query;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
 
 /**
  *
- * @author js.sosa10
+ * @author nd.munoz10
  */
 @Stateless
 public class VideoPersistence 
 {
-    private static final Logger LOGGER = Logger.getLogger(VideoPersistence.class.getName());
-
-    @PersistenceContext(unitName = "TechxplosionPU")
+    private static final Logger LOGGER = Logger.getLogger(BibliotecaPersistence.class.getName());
+    @PersistenceContext(unitName="TechxplosionPU")
     protected EntityManager em;
-    
-    public VideoEntity find(Long id) 
+    public VideoEntity find(Long id)
     {
-        LOGGER.log(Level.INFO, "Consultando company con id={0}", id);
-        return em.find(VideoEntity.class, id);
+        LOGGER.log(Level.INFO,"Consultando Video con id={0}",id);
+        return em.find(VideoEntity.class,id);
     }
-
-    public VideoEntity findByName(String name)
+    public List<VideoEntity> findAll()
     {
-        LOGGER.log(Level.INFO, "Consultando company con name = {0}", name);
-        TypedQuery<VideoEntity> q
-                = em.createQuery("select u from CompanyEntity u where u.name = :name", VideoEntity.class);
-        q = q.setParameter("name", name); 
-        return q.getSingleResult();
+      LOGGER.info("Consulatndo todas los videos");
+      Query q = em.createQuery("select u from VideoEntity u");
+      return q.getResultList();
     }
-
-    public List<VideoEntity> findAll() {
-        LOGGER.info("Consultando todos los companys");
-        javax.persistence.Query q = em.createQuery("select u from CompanyEntity u");
-        return q.getResultList();
-    }
-
-    public VideoEntity create(VideoEntity entity) {
-        LOGGER.info("Creando un company nuevo");
+    public VideoEntity create(VideoEntity entity)
+    {
+        LOGGER.info("Creando un video nueva");
         em.persist(entity);
-        LOGGER.info("Company creado");
+        LOGGER.info("Video creado");
         return entity;
     }
-
-    public VideoEntity update(VideoEntity entity) {
-        LOGGER.log(Level.INFO, "Actualizando company con id={0}", entity.getId());
+    public VideoEntity update(VideoEntity entity)
+    {
+        LOGGER.log(Level.INFO,"Actualizando Video con id={0}",entity.getId());
         return em.merge(entity);
     }
-
-    public void delete(Long id) {
-        LOGGER.log(Level.INFO, "Borrando company con id={0}", id);
-        VideoEntity entity = em.find(VideoEntity.class, id);
+    public void delete(Long id)
+    {
+        LOGGER.log(Level.INFO,"Borrando video con id={0}",id);
+        VideoEntity entity = em.find(VideoEntity.class,id);
         em.remove(entity);
     }
 }
