0e11af7ae4ebb7aa273e26721c7d7d71ace95468
js.numpaque10 <js.numpaque10@ISIS2603-05.sis.virtual.uniandes.edu.co>
2016-10-11 09:15:46 -0500

    Se comienzan a implementar los test para la clase RevservaPersistence

---
+++ bibliotecas-back/src/test/java/co/edu/uniandes/techxplosion/bibliotecas/persistence/ReservaPersistenceTest.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.techxplosion.bibliotecas.persistence;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import org.junit.Assert;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+/**
+ *
+ * @author js.numpaque10
+ */
+
+@RunWith(Arquillian.class)
+public class ReservaPersistenceTest {
+    
+    /**
+     * @return el jar que se desplegará para la prueba
+     */
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(DepartmentEntity.class.getPackage())
+                .addPackage(DepartmentPersistence.class.getPackage())
+                .addPackage(CompanyEntity.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+    
+    /**
+     * Libro al que están asociadas las reservas.
+     */
+    LibroEntity fatherEntity1;
+    
+    /**
+     * Video al que están asociadas las reservas.
+     */
+    VideoEntity fatherEntity2;
+    
+    /**
+     * Usuario al que están asociadas las reservas.
+     */
+    VideoEntity fatherEntity3;
+    
+    
+    
+    
+    
+}
