9286de4c8b422cfeaa83a75a483f39f0370a2168
Juan Sosa <js.sosa10@uninades.edu.co>
2016-10-18 11:44:35 -0500

    arreglos

--- bibliotecas-back/src/test/java/co/edu/uniandes/techxplosion/bibliotecas/test/persistence/BibliotecaPersistenceTest.java
+++ bibliotecas-back/src/test/java/co/edu/uniandes/techxplosion/bibliotecas/test/persistence/BibliotecaPersistenceTest.java
@@ -15,6 +15,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.UserTransaction;
 import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.After;
@@ -24,6 +25,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
 import uk.co.jemos.podam.api.PodamFactory;
 import uk.co.jemos.podam.api.PodamFactoryImpl;
 
@@ -31,6 +33,7 @@ import uk.co.jemos.podam.api.PodamFactoryImpl;
  *
  * @author js.sosa10
  */
+@RunWith(Arquillian.class)
 public class BibliotecaPersistenceTest {
       @Deployment
     public static JavaArchive createDeployment() {
@@ -52,7 +55,7 @@ public class BibliotecaPersistenceTest {
     @Inject
     UserTransaction utx;
      
-     private List<BibliotecaEntity> data = new ArrayList<BibliotecaEntity>();
+     private List<BibliotecaEntity> data = new ArrayList<>();
     
     @Before
     public void setUp() 
