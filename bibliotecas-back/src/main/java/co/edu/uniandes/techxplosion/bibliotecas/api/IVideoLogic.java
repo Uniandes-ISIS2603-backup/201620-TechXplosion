a4246553d9c4be6f7f1387c2c9d0d05f373e14f4
Nicolás Muñoz <nd.munoz10@uniandes.edu.co>
2016-11-01 07:06:39 -0500

    VideoLogic, IVideoLogic

---
+++ bibliotecas-back/src/main/java/co/edu/uniandes/techxplosion/bibliotecas/api/IVideoLogic.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.techxplosion.bibliotecas.api;
+
+import co.edu.uniandes.techxplosion.bibliotecas.entities.VideoEntity;
+import java.util.List;
+
+/**
+ *
+ * @author nd.munoz10
+ */
+public interface IVideoLogic 
+{
+    public List<VideoEntity> getVideos();
+    public VideoEntity getVideo(Long id);
+    public List<VideoEntity> getVideoPorUsuario(Long idUsuario);
+    public VideoEntity createVideo(VideoEntity entity) throws Exception;
+    public VideoEntity updateVideo(VideoEntity entity);
+    public void deleteVideo(Long id);
+    
+}
