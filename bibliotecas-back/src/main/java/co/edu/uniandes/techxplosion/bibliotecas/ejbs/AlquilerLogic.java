a5098bc6eed28a22dd04b30299d36b4216eb74c2
sapardo10 <sa.pardo10@ISIS2603-04.sis.virtual.uniandes.edu.co>
2016-11-01 00:19:31 -0500

    AlquilerLogic getAlquileres

--- bibliotecas-back/src/main/java/co/edu/uniandes/techxplosion/bibliotecas/ejbs/AlquilerLogic.java
+++ bibliotecas-back/src/main/java/co/edu/uniandes/techxplosion/bibliotecas/ejbs/AlquilerLogic.java
@@ -5,11 +5,58 @@
  */
 package co.edu.uniandes.techxplosion.bibliotecas.ejbs;
 
+import co.edu.uniandes.techxplosion.bibliotecas.api.IAlquilerLogic;
+import co.edu.uniandes.techxplosion.bibliotecas.entities.AlquilerEntity;
+import co.edu.uniandes.techxplosion.bibliotecas.persistence.AlquilerPersistence;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
 /**
  *
  * @author sa.pardo10
  */
-public class AlquilerLogic 
+@Stateless
+public class AlquilerLogic implements IAlquilerLogic
 {
+
+    @Inject
+    private AlquilerPersistence persistence;
+    
+    @Override
+    public List<AlquilerEntity> getAlquileres() 
+    {
+        return persistence.findAll();
+    }
+
+    @Override
+    public AlquilerEntity getAlquiler(Long id) 
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<AlquilerEntity> getAlquilerPorUsuario(Long idUsuario) 
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public AlquilerEntity createAlquiler(AlquilerEntity entity) throws Exception 
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public AlquilerEntity updateAlquiler(AlquilerEntity entity) 
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deleteAlquiler(Long id) 
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
