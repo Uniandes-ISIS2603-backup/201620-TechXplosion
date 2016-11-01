2121c0388a13179ca2e9d9db4b646b92c610cd8a
sapardo10 <sa.pardo10@ISIS2603-04.sis.virtual.uniandes.edu.co>
2016-11-01 00:15:41 -0500

    IAlquilerLogic

--- bibliotecas-back/src/main/java/co/edu/uniandes/techxplosion/bibliotecas/api/IAlquilerLogic.java
+++ bibliotecas-back/src/main/java/co/edu/uniandes/techxplosion/bibliotecas/api/IAlquilerLogic.java
@@ -5,11 +5,20 @@
  */
 package co.edu.uniandes.techxplosion.bibliotecas.api;
 
+import co.edu.uniandes.techxplosion.bibliotecas.entities.AlquilerEntity;
+import java.util.List;
+
 /**
  *
  * @author sa.pardo10
  */
 public interface IAlquilerLogic 
 {
+    public List<AlquilerEntity> getAlquileres();
+    public AlquilerEntity getAlquiler(Long id);
+    public List<AlquilerEntity> getAlquilerPorUsuario(Long idUsuario);
+    public AlquilerEntity createAlquiler(AlquilerEntity entity) throws Exception;
+    public AlquilerEntity updateAlquiler(AlquilerEntity entity);
+    public void deleteAlquiler(Long id);
     
 }
