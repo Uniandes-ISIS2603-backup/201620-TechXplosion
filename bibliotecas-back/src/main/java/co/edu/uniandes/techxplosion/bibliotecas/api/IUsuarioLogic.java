4f60b5a458e8b76901294305f609a9806d495ce6
jm.rodriguez11 <jm.rodriguez11@ISIS2603-0107.sis.virtual.uniandes.edu.co>
2016-11-03 12:08:39 -0500

    added items

---
+++ bibliotecas-back/src/main/java/co/edu/uniandes/techxplosion/bibliotecas/api/IUsuarioLogic.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.techxplosion.bibliotecas.api;
+
+import co.edu.uniandes.techxplosion.bibliotecas.entities.AlquilerEntity;
+import co.edu.uniandes.techxplosion.bibliotecas.entities.MedioPagoEntity;
+import co.edu.uniandes.techxplosion.bibliotecas.entities.ReservaEntity;
+import co.edu.uniandes.techxplosion.bibliotecas.entities.UsuarioEntity;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author jm.rodriguez11
+ */
+
+public interface IUsuarioLogic 
+{
+    public List<AlquilerEntity> getAlquileres(Long usuarioId);
+    public List<ReservaEntity> getReservas(Long usuarioId);
+    public UsuarioEntity getUsuario(Long usuarioId);
+    public UsuarioEntity createUsuario(UsuarioEntity entity) throws Exception;
+    
+    public UsuarioEntity updateUsuario(UsuarioEntity entity) ;
+    public void deleteUsuario(Long id);  
+   public List<MedioPagoEntity> getMedioPago();
+   
+    
+   
+}
