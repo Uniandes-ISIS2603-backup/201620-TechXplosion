e4af15c6ca426aae7a4be053244b95b8153b573b
juan camilo sanchez <jc.sanchez@uniandes.edu.co>
2016-11-08 11:12:33 -0500

    libro

--- bibliotecas-back/src/test/java/co/edu/uniandes/techxplosion/bibliotecas/test/persistence/UsuarioPersistenceTest.java
+++ bibliotecas-back/src/test/java/co/edu/uniandes/techxplosion/bibliotecas/test/persistence/UsuarioPersistenceTest.java
@@ -13,6 +13,17 @@ import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.UserTransaction;
+import static org.glassfish.api.admin.Supplemental.Timing.Before;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
 
 /**
  *
