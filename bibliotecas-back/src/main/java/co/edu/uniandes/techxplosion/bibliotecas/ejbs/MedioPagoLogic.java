ee37db794cbe00cfe3723b0fcf9a0d80982ac9f4
sapardo10 <sa.pardo10@ISIS2603-04.sis.virtual.uniandes.edu.co>
2016-11-16 11:45:57 -0500

    MedioPagoLogic

---
+++ bibliotecas-back/src/main/java/co/edu/uniandes/techxplosion/bibliotecas/ejbs/MedioPagoLogic.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.techxplosion.bibliotecas.ejbs;
+
+import co.edu.uniandes.techxplosion.bibliotecas.api.IMedioPagoLogic;
+import co.edu.uniandes.techxplosion.bibliotecas.entities.BibliotecaEntity;
+import co.edu.uniandes.techxplosion.bibliotecas.entities.MedioPagoEntity;
+import co.edu.uniandes.techxplosion.bibliotecas.persistence.MedioPagoPersistence;
+import java.util.List;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author sa.pardo10
+ */
+public class MedioPagoLogic implements IMedioPagoLogic
+{
+    @Inject
+    private MedioPagoPersistence persistence;
+
+    @Override
+    public List<MedioPagoEntity> getMedioPago() 
+    {
+        return persistence.findAll();
+    }
+
+    @Override
+    public MedioPagoEntity getMedioPago(Long id) 
+    {
+        return persistence.find(id);
+    }
+
+    @Override
+    public MedioPagoEntity createMedioPago(MedioPagoEntity entity) throws Exception 
+    {
+        MedioPagoEntity alreadyExist = getMedioPago(entity.getId());
+        if (alreadyExist != null) 
+        {
+            throw new Exception("Ya existe un MedioPago con ese id");
+        } 
+        else
+        {
+            persistence.create(entity);
+        }
+        return entity;
+    }
+
+    @Override
+    public MedioPagoEntity updateMedioPago(MedioPagoEntity entity) 
+    {
+        return persistence.update(entity);
+    }
+
+    @Override
+    public void deleteMedioPago(Long id) 
+    {
+        persistence.delete(id);
+    }
+    
+    
+    
+}
